7570c8ff5a985274cdf4a760f839991c2c54765f
= <=>
2023-06-27 16:26:10 -0300

    master - more code

--- src/app/services/glider/mock-gliders.ts
+++ src/app/services/glider/mock-gliders.ts
@@ -4,29 +4,36 @@ export const GLIDERS: Glider[] = [
     {
         model: "Grob G103 Twin Astir II",
         designator: "LV-EJB",
-        image: "./assets/LV-EJB/photo.png",
-        manualList:[{name:"Manual de vuelo",link:"https://drive.google.com/file/d/1qhl45ohaZ9qaEeeAyUp-xEWh18J9zIlb/view?usp=drive_link"}],
+        image: "https://drive.google.com/uc?id=1scGfbrgTqsvfgcE4bILKANzZeVhEBEdV",
+        manualList:[{name:"Manual de vuelo",link:"https://drive.google.com/file/d/1Vw0pVjjdougZ-falPRtVRE52i7x20rxF/view?usp=drive_link"}],
         description: "Planeador biplaza utilizado para bautismos, escuela y vuelos deportivos."
       },
       {
         model: "ICA Is282B",
         designator: "LV-EJP",
-        image: "./assets/LV-EJP/LV-EJP.png",
-        manualList:[{name:"Manual de vuelo",link:"./assets/LV-EJP/IS28b2 LV-EJP.pdf"}],
+        image: "https://drive.google.com/uc?id=1SxzhToRHB4nuigX-TKfnoL1n4j2YZCLj",
+        manualList:[{name:"Manual de vuelo",link:"https://drive.google.com/file/d/1nsh3AE_l1dghLO6XHg3Aj8sHTt5zEGOb/view?usp=drive_link"}],
         description: "Planeador biplaza utilizado para bautismos, escuela y vuelos deportivos."
       },
       {
         model: "Smyk Pw-5",
         designator: "LV-EMW",
-        image: "./assets/LV-EMW/LV-EMW.png",
-        manualList:[{name:"Manual de vuelo (no oficial)",link:"./assets/LV-EMW/PW5 Manual de Vuelo (no oficial).pdf"}],
-        description: "Planeador clase mundo que se usa para x y j y asdf"
+        image: "https://drive.google.com/uc?id=1KyqBm_If-1DWm23VUQFGKs7byO4ocbBI",
+        manualList:[{name:"Manual de vuelo (no oficial)",link:"https://drive.google.com/file/d/168jzWGnvX2LBjrFDqvTuAvZvkMyxRTDx/view?usp=sharing"}],
+        description: "Planeador clase mundo, afectado a la escuela de vuelo."
       },
       {
         model: 'Grob G102 Astir Cs77 "59"',
         designator: "LV-DNH",
+        image: "https://drive.google.com/uc?id=1oJVlg-yQLkybXWR2JnZk8d8iFLpz91ra",
+        manualList:[{name:"Manual de vuelo",link:"https://drive.google.com/file/d/1tTe-OKSdZJXv8GBs9RvYH24e8ORNuR_P/view?usp=drive_link"}],
+        description: "Planeador clase club."
+      },
+      {
+        model: 'Aero Boero 180 ',
+        designator: "LV-AOG",
         image: "",
         manualList:[],
-        description: "Planeador clase club que se usa para x y j y asdf"
+        description: "Remolcador afectado al club de planeadores Tandil."
       }
 ];
