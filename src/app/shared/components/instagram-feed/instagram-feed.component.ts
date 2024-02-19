2fe29fdabdc11b7651a10462b81b53cbf4c7dfc5
= <=>
2024-02-19 20:32:22 -0300

    instagram feed added

--- src/app/shared/components/instagram-feed/instagram-feed.component.ts
+++ src/app/shared/components/instagram-feed/instagram-feed.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-instagram-feed',
   templateUrl: './instagram-feed.component.html',
-  styleUrls: ['./instagram-feed.component.css'],
+  styleUrls: ['./instagram-feed.component.scss'],
 })
 export class InstagramFeedComponent {}
