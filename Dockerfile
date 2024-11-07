3fa95a48429972a10e215f8c7077829f5f71261f
Matias Rodriguez <mrodriguez@exa.unicen.edu.ar>
2024-11-06 21:05:11 -0300

    Angular update 2.0 (#1)
    * update
    
    * update
    
    * d
    
    * 1
    
    * 1
    
    * 1
    
    * update 18
    
    * update
    
    * a
    
    * 1
    
    * g
    
    * a
    
    * a
    
    * hb
    
    * l
    
    * a
    
    * angular update and visual changes v2.0
    
    ---------
    
    Co-authored-by: = <=>

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:20.10.0 as build
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+RUN npm install -g @angular/cli
+
+COPY . .
+
+RUN ng build --configuration=production
+
+FROM nginx:latest
+
+COPY --from=build app/dist/cpt-web /usr/share/nginx/html
+
+EXPOSE 80
--- src/app/pages/welcome/image-parallax/image-parallax.component.ts
+++ src/app/pages/welcome/image-parallax/image-parallax.component.ts
@@ -6,5 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./image-parallax.component.scss'],
 })
 export class ImageParallaxComponent {
-  @Input() text = '¡Bienvenido al sitio web del club, aqui encontraras toda la informacion necesaria para realizar la actividad!';
+  @Input() text = 'Club de planeadores Tandil';
 }
